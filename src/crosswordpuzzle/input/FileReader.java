d7650cfb4532c1c73c3377751d56501547cd685a
Chidi NWeke <chidi.nweke@element61.be>
2021-12-25 20:06:06 +0100

    FileReader class works

--- src/crosswordpuzzle/input/FileReader.java
+++ src/crosswordpuzzle/input/FileReader.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
 public class FileReader {
     /*
     This class is responsible for reading the input file and storing its information in a handful of variables.
+    The key idea of this class is that the scanner is passed between functions.
      */
-    private int horizontal;
-    private int vertical;
-    private char[][] tiles;
-    private String inputPath;
+    private int horizontalDimension;
+    private int verticalDimension;
+    private String[][] tiles = {};
+    private final String inputPath;
+    private String descriptiveText = null;
 
     public FileReader() {
-        /*
+         /*
         The default constructor sets the input path to puzzle1
          */
         this.inputPath = Path.of("src", "crosswordpuzzle", "input", "puzzle-1.txt").toAbsolutePath().toString();
@@ -29,34 +31,84 @@ public class FileReader {
         this.inputPath = Path.of("src", "crosswordpuzzle", "input", puzzleFile).toAbsolutePath().toString();
     }
 
-    private void readDimensions() {
+    public void readFile(){
+        try(Scanner scan = new Scanner(new File(inputPath))){
+            readDimensions(scan);
+            readTiles(scan);
+            readDescriptiveText(scan);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("No file was found at the current path: " + inputPath + ". Are you sure it is present?");
+        }
+    }
+
+    private void readDimensions(Scanner scan) {
         /*
-        Reads in the dimensions (horizontal and vertical of the first file.
+        Reads in the dimensions (horizontal and vertical) of the first file.
+        The scanner is not closed in the 'finally' block as it is passed to the next function.
          */
-        try (Scanner scan = new Scanner(new File(inputPath))) {
-            int vertical = scan.nextInt();
-            int horizontal = scan.nextInt();
-        } catch (FileNotFoundException e) {
-            System.out.println("No file was found at the current path: " + inputPath +". Are you sure it is present?");
+        try {
+            verticalDimension = scan.nextInt();
+            horizontalDimension = scan.nextInt();
         }
         catch (InputMismatchException e) {
             System.out.println("The dimensions provided should be integer, are you sure the right file was provided?");
         }
     }
-    public void readTable(Scanner scan){
+
+    private void readTiles(Scanner scan){
         /*
          Takes the previously made scanner and parses the table based on the dimensions.
          */
-        for (int i = 0; i < horizontal; i++){
-            for (int j = 0; j < vertical; j++){
-
+        tiles = new String[horizontalDimension][verticalDimension];
+        try{
+            for (int i = 0; i < horizontalDimension; i++){
+                for (int j = 0; j < verticalDimension; j++){
+                    tiles[i][j] = scan.next();
+                }
             }
         }
-
+        catch (InputMismatchException e) {
+            System.out.println("The data provided is in an incorrect format.");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Failed to read in the table. Make sure the dimensions are correct.");
+        }
     }
 
+    private void readDescriptiveText(Scanner scan){
+        /*
+        Reads the descriptive text from the file and stores it in a single String.
+         */
+        try{
+            descriptiveText = scan.useDelimiter("\\Z").next();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("The data provided is in an incorrect format.");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Failed to read in the descriptive text. Make sure the dimensions are correct.");
+        }
+    }
 
+    public int getHorizontalDimension() {
+        return horizontalDimension;
+    }
 
+    public int getVerticalDimension() {
+        return verticalDimension;
+    }
 
+    public String[][] getTiles() {
+        return tiles;
+    }
 
+    public String getDescriptiveText() {
+        return descriptiveText;
+    }
 }
+
+
+
+
+
